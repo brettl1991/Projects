408793b4e8d11b23f0d7b4239a9921314d28b060
Agnes <brettl1991@gmail.com>
2021-08-15 15:21:16 +0100

    Create a function which allow the page to starting fully opaque and going to start to fade out as it loads up

--- script.js
+++ script.js
@@ -3,15 +3,22 @@ const bg = document.querySelector(".bg");
 
 let load = 0;
 
-let int = setInterval(blurring, 30)
+let int = setInterval(blurring, 30);
 
 function blurring() {
   load++;
 
-    if(load > 99) {
-      clearInterval(int)
-    }
+  if (load > 99) {
+    clearInterval(int);
+  }
   // console.log(load);
 
-  loadText.innerText = `${load}%`
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+//map a range of numbers to another range of numbers
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
