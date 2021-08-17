669f3b7b7d5e4e6f4a109809ba1505daca217566
brettl1991 <brettl1991@gmail.com>
2021-08-17 14:28:31 +0100

    Adding the active slide index to our page

---
+++ DoubleVerticleSlider/script.js
@@ -0,0 +1,11 @@
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slidesLenght = slideRight.querySelectorAll('div').length
+
+//Active slide index
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLenght - 1) * 100}vh`
