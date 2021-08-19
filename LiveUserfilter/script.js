dbbf32c256bf86c40603187a9d0a93fc04e3020b
brettl1991 <brettl1991@gmail.com>
2021-08-19 16:07:59 +0100

    Fetching trough the data and constract the list item for each of these users and push it into our HTML

--- LiveUserfilter/script.js
+++ LiveUserfilter/script.js
@@ -14,7 +14,19 @@ async function getData() {
     //Clear results
     results.innerHTML = ''
 
-    
+    results.forEach(user => {
+        const li = document.createElement('li')
+
+        listItem.push(li)
+
+        li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}">
+            <div class="user-info">
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
+        `
+    })
 }
 
 
